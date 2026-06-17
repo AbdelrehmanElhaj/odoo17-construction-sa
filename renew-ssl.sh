1bf7ead92cd96991e88ef7cbb73639f9b23a506e
Ubuntu <ubuntu@ip-172-31-19-224.eu-north-1.compute.internal>
2026-06-17 18:43:31 +0000

    server configuration

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -8,8 +8,8 @@ echo "[$(date)] Running certbot renew..."
 $COMPOSE run --rm --entrypoint certbot certbot renew --quiet
 
 echo "[$(date)] Installing renewed certs..."
-sudo cp "$ROOT/config/certbot/certs/live/sms.hdrelhaj.com/fullchain.pem" "$ROOT/config/certs/nginx.crt"
-sudo cp "$ROOT/config/certbot/certs/live/sms.hdrelhaj.com/privkey.pem"   "$ROOT/config/certs/nginx.key"
+sudo cp "$ROOT/config/certbot/certs/live/csm.hdrelhaj.com/fullchain.pem" "$ROOT/config/certs/nginx.crt"
+sudo cp "$ROOT/config/certbot/certs/live/csm.hdrelhaj.com/privkey.pem"   "$ROOT/config/certs/nginx.key"
 sudo chown "$(id -u):$(id -g)" "$ROOT/config/certs/nginx.crt" "$ROOT/config/certs/nginx.key"
 
 docker exec odoo17-nginx nginx -s reload
